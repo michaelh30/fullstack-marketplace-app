c5356a391308e7d5f9b0510cf6696f622a6dd546
michaelh30 <michael.hermawan@banksinarmas.com>
2026-04-24 15:39:04 +0700

    feat: template project market place

---
+++ backend/pp-marketplace/src/main/java/com/example/pp_marketplace/PpMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pp_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpMarketplaceApplication.class, args);
+	}
+
+}
